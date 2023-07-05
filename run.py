d9af02a7dc98b754e1f6518b25a9ab846f1d14a0
Abhilash Aynipully <abhidigitally@gmail.com>
2023-07-05 11:28:01 +0000

    Create welcome_and_instructions function to print info at start

--- run.py
+++ run.py
@@ -13,3 +13,19 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('mental_math')
 
+def welcome_and_instructions():
+    """
+    Prints welcome message on console
+    Prints instructions for the user to follow
+    """
+    print("Welcome to Mental Math Game!!!")
+    print("-------------------------------")
+    print("-------------------------------\n")
+    time.sleep(1.5)
+    print("Please read the below instructions carefully :")
+    print("**********************************************\n")
+    print("- There will be a total of 10 questions\n")
+    print("- Each question will have 4 options 'A','B','C,'D'\n")
+    print("- To choose option 'A' as answer type A or a and press enter\n")
+    print("- There are no negative marking for wrong answers\n")
+
